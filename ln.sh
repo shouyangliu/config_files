5986d44234e18ee385d2e4a49c26945aa2c80d4d
yarrow <tylerliuyang@outlook.com>
2025-02-24 22:32:37 +0800

    fix something

--- ln.sh
+++ ln.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ln -s -r nvim/ ~/.config/
 #ln -s -r wezterm/ ~/.config/
-ln -s ~/my_config/config_files/tmux/.tmux.conf ~/.tmux.conf
-ln -s ~/my_config/config_files/picom/picom.conf ~/.picom.conf
-ln -s /home/y/my_config/config_files/i3-gaps/config ~/.config/i3/config
-ln -s /home/yl/my_config/config_files/alacritty ~/.config/alacritty
+ln -s ~/config_files/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/config_files/picom/picom.conf ~/.picom.conf
+ln -s ~/config_files/i3-gaps/config ~/.config/i3/config
+ln -s ~/config_files/alacritty ~/.config/alacritty
+ln -s ~/config_files/kitty/ ~/.config/kitty/
