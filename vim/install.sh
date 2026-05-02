fe1f4c4b152584990c5a935c17564b8892fb7829
yarrow <tylerliuyang@outlook.com>
2026-05-02 22:11:38 +0800

    dlete vim

--- vim/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Vim installation is disabled - using LazyVim (Neovim) instead
-echo "Vim installation skipped - using LazyVim (Neovim)"
-echo "If you want to install vim manually, use: sudo apt install vim"
