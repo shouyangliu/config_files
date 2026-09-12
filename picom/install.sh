18958401a1c5eb71385dcf8b30418120cb341eac
yarrow <tylerliuyang@outlook.com>
2026-09-12 23:11:44 +0800

    delete unless file

--- picom/install.sh
+++ picom/install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-cd "$(dirname "$0")"
-
-git submodule update --init --recursive
-meson setup --buildtype=release . build
-ninja -C build
-sudo ninja -C build install
-
+sudo apt install -y picom
 echo "picom installed."
--- remove_invalid_ppa.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Remove invalid jonathonf/vim PPA
-echo "Removing invalid jonathonf/vim PPA..."
-sudo rm -f /etc/apt/sources.list.d/jonathonf-ubuntu-vim-noble.sources
-sudo apt update
-echo "Done! Invalid PPA removed."
