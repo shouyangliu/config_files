4bb33e7ac5b03b4ac653e0d4a9597c65ee5ff0c2
yarrow <tylerliuyang@outlook.com>
2026-09-12 23:50:21 +0800

    picom: 添加 FT-Labs/picom 源码到本地

---
+++ picom/source/src/atom.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <xcb/xcb.h>
+
+#include "cache.h"
+#include "meta.h"
+
+// clang-format off
+// Splitted into 2 lists because of the limitation of our macros
+#define ATOM_LIST1 \
+	_NET_WM_WINDOW_OPACITY, \
+	_NET_FRAME_EXTENTS, \
+	WM_STATE, \
+	_NET_WM_NAME, \
+	_NET_WM_PID, \
+	WM_NAME, \
+	WM_CLASS, \
+	WM_ICON_NAME, \
+	WM_TRANSIENT_FOR, \
+	WM_WINDOW_ROLE, \
+	WM_CLIENT_LEADER, \
+	WM_CLIENT_MACHINE, \
+	_NET_ACTIVE_WINDOW, \
+	_COMPTON_SHADOW, \
+	COMPTON_VERSION, \
+	_NET_WM_WINDOW_TYPE, \
+	_XROOTPMAP_ID, \
+	ESETROOT_PMAP_ID, \
+	_XSETROOT_ID
+
+#define ATOM_LIST2 \
+	_NET_WM_WINDOW_TYPE_DESKTOP, \
+	_NET_WM_WINDOW_TYPE_DOCK, \
+	_NET_WM_WINDOW_TYPE_TOOLBAR, \
+	_NET_WM_WINDOW_TYPE_MENU, \
+	_NET_WM_WINDOW_TYPE_UTILITY, \
+	_NET_WM_WINDOW_TYPE_SPLASH, \
+	_NET_WM_WINDOW_TYPE_DIALOG, \
+	_NET_WM_WINDOW_TYPE_NORMAL, \
+	_NET_WM_WINDOW_TYPE_DROPDOWN_MENU, \
+	_NET_WM_WINDOW_TYPE_POPUP_MENU, \
+	_NET_WM_WINDOW_TYPE_TOOLTIP, \
+	_NET_WM_WINDOW_TYPE_NOTIFICATION, \
+	_NET_WM_WINDOW_TYPE_COMBO, \
+	_NET_WM_WINDOW_TYPE_DND, \
+	_NET_WM_STATE, \
+	_NET_WM_STATE_FULLSCREEN, \
+	_NET_WM_BYPASS_COMPOSITOR, \
+	UTF8_STRING, \
+	C_STRING
+// clang-format on
+
+#define ATOM_DEF(x) xcb_atom_t a##x
+
+struct atom_entry;
+struct atom {
+	struct cache c;
+	LIST_APPLY(ATOM_DEF, SEP_COLON, ATOM_LIST1);
+	LIST_APPLY(ATOM_DEF, SEP_COLON, ATOM_LIST2);
+};
+
+/// Create a new atom object with a xcb connection. `struct atom` does not hold
+/// a reference to the connection.
+struct atom *init_atoms(xcb_connection_t *c);
+
+xcb_atom_t get_atom(struct atom *a, const char *key, xcb_connection_t *c);
+xcb_atom_t get_atom_cached(struct atom *a, const char *key);
+
+void destroy_atoms(struct atom *a);
---
+++ picom/source/src/cache.c
@@ -0,0 +1,43 @@
+#include <uthash.h>
+
+#include "cache.h"
+
+struct cache_handle *cache_get(struct cache *c, const char *key) {
+	struct cache_handle *e;
+	HASH_FIND_STR(c->entries, key, e);
+	return e;
+}
+
+int cache_get_or_fetch(struct cache *c, const char *key, struct cache_handle **value,
+                       void *user_data, cache_getter_t getter) {
+	*value = cache_get(c, key);
+	if (*value) {
+		return 0;
+	}
+
+	int err = getter(c, key, value, user_data);
+	assert(err <= 0);
+	if (err < 0) {
+		return err;
+	}
+	(*value)->key = strdup(key);
+
+	HASH_ADD_STR(c->entries, key, *value);
+	return 1;
+}
+
+static inline void
+cache_invalidate_impl(struct cache *c, struct cache_handle *e, cache_free_t free_fn) {
+	free(e->key);
+	HASH_DEL(c->entries, e);
+	if (free_fn) {
+		free_fn(c, e);
+	}
+}
+
+void cache_invalidate_all(struct cache *c, cache_free_t free_fn) {
+	struct cache_handle *e, *tmpe;
+	HASH_ITER(hh, c->entries, e, tmpe) {
+		cache_invalidate_impl(c, e, free_fn);
+	}
+}
