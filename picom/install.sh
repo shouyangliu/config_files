30269b4e3b6e2cc471d551cafca57e090af0616c
yarrow <tylerliuyang@outlook.com>
2026-09-12 23:33:50 +0800

    add picom

--- picom/install.sh
+++ picom/install.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 set -e
 
-sudo apt install -y picom
-echo "picom installed."
+PICOM_BIN="/usr/local/bin/picom"
+PICOM_REPO="https://github.com/FT-Labs/picom.git"
+BUILD_DIR="/tmp/picom-build"
+
+if command -v picom &>/dev/null; then
+    echo "picom already installed: $(picom --version 2>&1)"
+    exit 0
+fi
+
+echo "Installing build dependencies..."
+sudo apt install -y \
+    libx11-dev libx11-xcb-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev \
+    libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev \
+    libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev \
+    libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev \
+    libev-dev libepoxy-dev uthash-dev meson ninja-build git
+
+echo "Cloning FT-Labs/picom..."
+rm -rf "$BUILD_DIR"
+git clone --depth 1 "$PICOM_REPO" "$BUILD_DIR"
+
+echo "Building picom..."
+cd "$BUILD_DIR"
+meson setup --buildtype=release --prefix=/usr/local build
+ninja -C build
+
+echo "Installing picom..."
+sudo ninja -C build install
+
+rm -rf "$BUILD_DIR"
+echo "picom installed: $(picom --version 2>&1)"
